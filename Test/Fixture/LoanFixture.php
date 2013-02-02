26b6d00f45a39da1527110d028617f3e58112c6f
Manuel Viloria <mviloriameza@gmail.com>
2013-02-02 10:50:50 -0500

    MVC

---
+++ Test/Fixture/LoanFixture.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * LoanFixture
+ *
+ */
+class LoanFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'book_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'user_id' => 1,
+			'book_id' => 1,
+			'created' => '2013-02-02 15:01:51',
+			'modified' => '2013-02-02 15:01:51'
+		),
+	);
+
+}
