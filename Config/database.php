371c725a782dced18b9c8368e5fc7e8813020ffd
Manuel Viloria <mviloriameza@gmail.com>
2013-02-02 09:55:48 -0500

    Initial Commit

---
+++ Config/database.php
@@ -0,0 +1,20 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'library',
+	);
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'librery_test',
+	);
+}
